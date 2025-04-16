5b4319efb0a5870e912e6134f47fb7533980f27b
Ваше Имя <_email@example.com>
2025-04-17 02:21:26 +0500

    add Command interface and Light + TurnOnLightCommand

---
+++ src/smarthome/Light.java
@@ -0,0 +1,12 @@
+package smarthome;
+
+public class Light {
+    public void turnOn() {
+        System.out.println("[Light] Включаю свет");
+    }
+
+    public void turnOff() {
+        System.out.println("[Light] Выключаю свет");
+    }
+}
+
